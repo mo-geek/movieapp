e2d8a2531855bafc09cd97f719e8adada28394ed
Atef <mohamedatef9495@gmail.com>
2022-07-23 03:08:21 +0200

    remaining files that includes the android && ios build_gradles

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:movieapp/core/constant/const.dart';
+import 'package:movieapp/core/utility/db.dart';
 import 'package:movieapp/ui/views/home_page.dart';
+import 'app/locator.dart';
+import 'core/utility/shared_pref.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Preferences.init();
+  setupLocator();
+  Preferences.setString(Constant.authTokenKey, Constant.authToken);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'MovieAppAssessment',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
